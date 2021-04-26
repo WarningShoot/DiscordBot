4057957604322f6e6d60c167e5981e72de2f3b07
Damian Zajda <UL0234229@edu.uni.lodz.pl>
2021-04-27 00:44:07 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,41 @@
+const Discord = require("discord.js");
+const fs = require("fs");
+
+const json = require("./data.json");
+const obj = json;
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+	console.log("Ready!");
+});
+
+client.on("message", (msg) => {
+	if (msg.content === "ping") msg.reply("Pong");
+});
+
+client.on("voiceStateUpdate", (oldMember, newMember) => {
+	const oldId = oldMember.channelID;
+	const newId = newMember.channelID;
+	const userId = oldMember.id;
+	console.log("---------------------");
+
+	if (oldId === null) {
+		//join
+		if (!obj.hasOwnProperty(userId)) {
+			obj[userId] = [];
+		}
+		obj[userId].unshift({ joined: new Date(), leave: "" });
+	} else if (newId === null) {
+		//leave
+		obj[userId][0].leave = new Date();
+		const newJSON = JSON.stringify(obj);
+		fs.writeFile("data.json", newJSON, "utf8", (err) => {
+			if (err) return console.log(err);
+		});
+	}
+
+	console.log(obj);
+});
+
+client.login("ODM0NTQ3NzMyNjQ3MTE2ODMw.YICfJA.smnVq9BpKa5xR1XRlKiunL39tNA");
