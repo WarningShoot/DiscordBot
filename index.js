9996b601b104913f36f7f780595466bde924282b
Damian Zajda <UL0234229@edu.uni.lodz.pl>
2021-04-28 14:37:04 +0200

    implement database to bot

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 require("dotenv").config();
 const Discord = require("discord.js");
-const fs = require("fs");
-const { join } = require("path");
-
-const obj = require("./data.json");
+const DataStore = require("nedb");
+const db = new DataStore({ filename: "./database.db", autoload: true });
 
 const client = new Discord.Client();
 
@@ -12,8 +10,13 @@ client.once("ready", () => {
 });
 
 client.on("message", (msg) => {
-	if (msg.content === ".time" && msg.channel.id === "642713531992506369")
+	if (
+		msg.content === ".time" &&
+		(msg.channel.id === process.env.CHANNEL_KOMENDY ||
+			msg.channel.id === process.env.CHANNEL_TEST)
+	)
 		countTime(msg);
+	if (msg.content === ".checkid") console.log(msg.channel.id);
 });
 
 client.on("voiceStateUpdate", (oldMember, newMember) => {
@@ -23,42 +26,64 @@ client.on("voiceStateUpdate", (oldMember, newMember) => {
 
 	if (oldId === null) {
 		//join
-		if (!obj.hasOwnProperty(userId)) {
-			obj[userId] = [];
-		}
-		obj[userId].unshift({ joined: new Date(), leave: "" });
+		db.find({ userId }, (err, docs) => {
+			if (err) throw err;
+			if (docs.length === 0) {
+				const temp = {
+					userId,
+					joined: new Date(),
+					time: 0,
+					online: true,
+				};
+
+				db.insert(temp, (err) => {
+					if (err) throw err;
+				});
+			} else {
+				db.update(
+					{ userId },
+					{ $set: { joined: new Date(), online: true } },
+					{},
+					(err) => {
+						if (err) throw err;
+					}
+				);
+			}
+		});
 	} else if (newId === null) {
 		//leave
-		obj[userId][0].leave = new Date();
-		const newJSON = JSON.stringify(obj);
-		fs.writeFile("./data.json", newJSON, "utf8", (err) => {
-			if (err) return console.log(err);
+		db.find({ userId }, (err, docs) => {
+			if (err) throw err;
+			if (docs.length === 0) return;
+
+			let time = docs[0].time;
+			time += new Date() - new Date(docs[0].joined);
+			db.update({ userId }, { $set: { time, online: false } }, {}, (err) => {
+				if (err) throw err;
+			});
 		});
 	}
 });
 
 const countTime = (msg) => {
 	const userId = msg.author.id;
-	let time = 0;
-	if (!obj.hasOwnProperty(userId)) return;
+	db.find({ userId }, (err, docs) => {
+		if (err) throw err;
+		if (docs.length === 0) return;
+		let { time, online, joined } = docs[0];
 
-	for (let i = 0; i < obj[userId].length; i++) {
-		const joined = obj[userId][i].joined;
-		const leave = obj[userId][i].leave;
-
-		if (!leave) {
+		if (online) {
 			time += new Date() - new Date(joined);
-		} else {
-			time += new Date(leave) - new Date(joined);
 		}
-	}
-	const seconds = Math.floor(time / 1000);
-	const minutes = Math.floor(seconds / 60);
-	const hours = Math.floor(minutes / 60);
-	const days = Math.floor(hours / 24);
 
-	const answer = `Spędziłeś na serwerze ${days} dni, ${hours} godzin, ${minutes} minut, ${seconds} sekund`;
-	msg.reply(answer);
+		const seconds = Math.floor(time / 1000);
+		const minutes = Math.floor(seconds / 60);
+		const hours = Math.floor(minutes / 60);
+		const days = Math.floor(hours / 24);
+
+		const answer = `Spędziłeś na serwerze ${days} dni, ${hours} godzin, ${minutes} minut, ${seconds} sekund`;
+		msg.reply(answer);
+	});
 };
 
 client.login(process.env.DISCORD_TOKEN);
